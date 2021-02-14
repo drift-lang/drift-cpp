0187aa63959b1b441623bb1066188af21d131a2b
bingxio <3106740988@qq.com>
2021-02-14 16:26:40 +0800

    first commit.

---
+++ drift.cc
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2021 bingxio. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout <<
+              "The Drift Programming Language" << std::endl;
+    return 0;
+}
